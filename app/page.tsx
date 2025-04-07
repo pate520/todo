0ba4743aafdec4ebff514b0f5ea4fe4d67b7d8ae
pate520 <zhyf520@gmail.com>
2025-04-07 15:30:58 +0800

    Force new deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import TodoList from "./components/Todo";
 import { getAllTodos } from "@/db/queries/todo-queries";
 
+// 强制触发新的部署
 export const runtime = 'edge';
 
 export default async function Home() {
